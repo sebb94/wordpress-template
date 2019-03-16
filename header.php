0d1dd22281de81b0d74fc7521aa97e961780ef2a
Seba <seba@DESKTOP-NPTG7Q6.localdomain>
2019-03-16 14:30:48 +0100

    dodanie font awesome, dalsze prace nad headerem

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
         <meta charset="<?php bloginfo('charset');?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="profile" href="http://gmpg.org/xfn/11">
+
         <?php if( is_singular() && pings_open(get_queried_object() ) ):?>
             <link rel="pingback" href="<?php bloginfo('pingback_url');?>">
         <?php endif; ?>
@@ -25,8 +26,9 @@
                 <div class="header-container text-center background-image" style="background-image: url(<?php header_image();?>)">
 
                     <div class="header-content">
-                        <h1 class="sitle-title"><?php bloginfo('name');?></h1>
-                        <h2 class="sitle-description"><?php bloginfo('description');?></h2>
+                        <i class="fa fa-smile-wink"></i>
+                        <h1 class="site-title"><?php bloginfo('name');?></h1>
+                        <h2 class="site-description"><?php bloginfo('description');?></h2>
                     </div> 
 
                     <div class="nav-container">
--- inc/enqueue.php
+++ inc/enqueue.php
@@ -34,7 +34,10 @@ add_action('admin_enqueue_scripts', 'seba_load_admin_scripts');
 
 function seba_load_scripts(){
       wp_enqueue_style('bootstrap',get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.3.1', 'all');
-       wp_enqueue_style('seba',get_template_directory_uri() . '/css/seba.css', array('bootstrap'), '1.0.0', 'all');
+       wp_enqueue_style('seba',get_template_directory_uri() . '/css/seba.css', array('bootstrap','raleway'), '1.0.0', 'all');
+       wp_enqueue_style('raleway', 'https://fonts.googleapis.com/css?family=Raleway:200,300,400,500&amp;subset=latin-ext' , array('bootstrap'));
+       wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.7.2/css/all.css', array('bootstrap','raleway'));
+       
        /* jquery do stopki */
         wp_deregister_script( 'jquery' );
         wp_register_script('jquery', get_template_directory_uri() . '/js/query.js', false, '3.3.1',true);
