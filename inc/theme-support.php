888952f1240efe788b513ba3f47a42124ac16c7f
Seba <seba@DESKTOP-NPTG7Q6.localdomain>
2019-03-22 19:46:13 +0100

    audio post format - dodanie oraz ostylowanie

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -103,3 +103,11 @@ function seba_get_attachments(){
     endif;
     return $image_url;
 } 
+
+function seba_get_embeded_media($type = array()){
+
+           $content = do_shortcode( apply_filters('the_content',get_the_content()));
+            $embed = get_media_embedded_in_content($content, $type);
+            $output = str_replace('?visual=true','?visual=false',$embed[0]);
+               return $output;
+}
---
+++ template-parts/seba-content-audio.php
@@ -0,0 +1,32 @@
+<?php /*
+
+@package sebatheme
+Audio
+
+*/
+
+?>
+
+<article id="post-<?php the_ID();?>" <?php post_class('seba-format-audio');?>>
+    <header class="entry-header">
+
+        <?php the_title('<h1 class="entry-title"><a href="'. esc_url( get_permalink()) . '" rel="bookmark">', '</a></h1>');?>
+
+        <div class="entry-meta">
+
+            <?php echo seba_posted_meta();?>
+         
+        </div>
+    </header>
+ 
+    <div clsas="entry-content">
+     
+            <?php 
+                echo seba_get_embeded_media(array('audio','iframe'));
+            ?>
+    </div><!-- .entry-content -->
+
+    <footer class="entry-footer">
+            <?php echo seba_posted_footer();?>
+    </footer>
+</article>
