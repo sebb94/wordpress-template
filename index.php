f28bd2afba37ba88a80e6e15de485988033b50b4
Seba <seba@DESKTOP-NPTG7Q6.localdomain>
2019-04-01 18:58:57 +0100

    ajax infinite scroll - animacja odkrywania posta css, funkcja js do okrywania posta, fix miniaturek karuzeli ajax

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ get_header();
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
-        <div class="container seba-post-container">
+        <div class="container seba-posts-container">
         <?php 
             if( have_posts()):
           
