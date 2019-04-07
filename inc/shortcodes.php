e664600925430741f89acc2236a3fd89227896a6
Seba <seba@DESKTOP-NPTG7Q6.localdomain>
2019-04-07 11:59:10 +0200

    popover oraz tooltip za pomoca shortcoda

---
+++ inc/shortcodes.php
@@ -0,0 +1,73 @@
+<?php 
+/*
+
+@package sebatheme
+
+Shortcode options
+
+*/
+function seba_tooltip( $atts, $content = null){
+
+    // get the attr 
+    $atts = shortcode_atts(
+        array(
+            'placement' => 'top',
+            'title'     => '',     
+        ), 
+        $atts,
+        'tooltip'
+    );
+
+    // return HTML 
+
+    $title = ( $atts['title'] == '' ? $title = $content : $atts['title']);
+  
+
+    return '<span class="seba-tooltip" data-toggle="tooltip" data-placement="'. $atts['placement'].'" title="' . $title . '">'. $content .'</span>';
+
+ 
+}
+
+
+add_shortcode('tooltip', 'seba_tooltip');
+
+function seba_popover( $atts, $content = null){
+
+    // get the attr 
+    $atts = shortcode_atts(
+        array(
+            'placement' => 'top',
+            'title'     => '',   
+            'content'   => '',
+            'trigger'   => 'click', 
+        ), 
+        $atts,
+        'popover'
+    );
+
+    // return HTML 
+
+ 
+    return '<span class="seba-popover" data-toggle="popover" data-placement="'. $atts['placement'].'" title="' . $atts['title'] . '" data-content="' . $atts['content'] .  '" data-trigger="' . $atts['trigger'] .  '">'. $content .'</span>';
+
+ 
+}
+
+
+add_shortcode('popover', 'seba_popover');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
